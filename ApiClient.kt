294c1a6c97a335a3bf335043df5f0c143ed8c78d
Vy Le <levytniwork@gmail.com>
2025-03-04 23:13:12 +0700

    Update ApiClient.kt

--- ApiClient.kt
+++ ApiClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
     // Replace with your FastAPI server URL (e.g., from Swagger /docs)
-    private const val BASE_URL = "https://question-seeker-furthermore-relief.trycloudflare.com/"
+    private const val BASE_URL = "..."
     val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
